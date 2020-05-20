72b47f1a4a51b8a8e2e9912fd1280f953b0954cb
stphenZkang <qiaok9921@163.com>
2020-05-20 11:13:41 +0800

    第一天练习题，两数之和

---
+++ Week_01/leetcode-qk-java/src/main/java/com/leetcode/qiaok/day03/TwoSum.java
@@ -0,0 +1,59 @@
+package com.leetcode.qiaok.day03;
+
+import org.omg.CORBA.MARSHAL;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *  1. 两数之和
+ *  给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ *
+ *  你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
+ *
+ *  示例:
+ *
+ *  给定 nums = [2, 7, 11, 15], target = 9
+ *
+ *  因为 nums[0] + nums[1] = 2 + 7 = 9
+ *  所以返回 [0, 1]
+ * @since 2020-05-20
+ * @author qiaok
+ */
+public class TwoSum {
+
+    public static void main(String[] args){
+        int[] nums = { 3,2,4 };
+        int target = 9;
+        TwoSum test = new TwoSum();
+        long start = System.currentTimeMillis();
+        int[] res = test.twoSum(nums,target);
+        System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
+        System.out.println("res="+ Arrays.toString(res));
+    }
+
+    /**
+     * hashmap
+     * 时间复杂度：
+     * 空间复杂度：
+     * @param nums
+     * @param target
+     * @return
+     */
+    private int[] twoSum(int[] nums, int target) {
+        Map<Integer,Integer> m = new HashMap<Integer, Integer>();
+        for (int i = 0; i < nums.length; i++) {
+            m.put(nums[i],i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int n = target - nums[i];
+            if(m.containsKey(n)&&m.get(n)!=i){
+                return new int[]{i,m.get(n)};
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
+
+}
