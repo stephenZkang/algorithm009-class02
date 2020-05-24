9db0064eef986ccb9283a42a0070b5da3210a943
stephenZkang <540079921@qq.com>
2020-05-24 20:59:05 +0800

    70. 爬楼梯

---
+++ Week_01/leetcode-qk-java/src/main/java/com/leetcode/qiaok/day07/ClimbStairs.java
@@ -0,0 +1,72 @@
+package com.leetcode.qiaok.day07;
+
+import com.leetcode.qiaok.day05.MaxArea;
+
+/**
+ * 70. 爬楼梯
+ * 假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+ *
+ * 每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+ *
+ * 注意：给定 n 是一个正整数。
+ *
+ * 示例 1：
+ *
+ * 输入： 2
+ * 输出： 2
+ * 解释： 有两种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶
+ * 2.  2 阶
+ * 示例 2：
+ *
+ * 输入： 3
+ * 输出： 3
+ * 解释： 有三种方法可以爬到楼顶。
+ * 1.  1 阶 + 1 阶 + 1 阶
+ * 2.  1 阶 + 2 阶
+ * 3.  2 阶 + 1 阶
+ *
+ * @since 2020-05-24
+ * @author qiaok
+ */
+public class ClimbStairs {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args){
+        ClimbStairs test = new ClimbStairs();
+        int n = 2;
+        long start = System.currentTimeMillis();
+        int res = test.climbStairs(n);
+        System.out.println("耗时"+(System.currentTimeMillis() - start)+"毫秒");
+        System.out.println("res=" + res);
+    }
+
+    /**
+     * 记忆递归
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(n)
+     * @param n
+     * @return
+     */
+    private int climbStairs(int n) {
+        int memo[] = new int[n + 1];
+        return climb_Stairs(0, n, memo);
+    }
+
+    public int climb_Stairs(int i, int n, int memo[]) {
+        if (i > n) {
+            return 0;
+        }
+        if (i == n) {
+            return 1;
+        }
+        if (memo[i] > 0) {
+            return memo[i];
+        }
+        memo[i] = climb_Stairs(i + 1, n, memo) + climb_Stairs(i + 2, n, memo);
+        return memo[i];
+    }
+
+}
